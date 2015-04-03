931d35c8e7f977b2b27cb0416dea364df93b3a02
kenrward <kenrward@gmail.com>
2015-04-03 15:12:57 -0400

    Added gitignore

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Vagrant-Puppet-GeoServer
 Vagrant and puppet files to install Tomcat and launch GeoServer.war
+**This config is still under development and is not ready **
+
--- manifests/default.pp
+++ manifests/default.pp
@@ -2,7 +2,7 @@ exec { "apt-update":
     command => "/usr/bin/apt-get update"
 }
 
-package { 'vim':
+package { 'vim-tiny':
 	ensure => installed,
 	}
 	
@@ -15,6 +15,6 @@ service { 'tomcat6':
 
 file { '/var/lib/tomcat6/webapps/geoserver-2.7.0.war':
     owner => 'root',
-    source => 'puppet:///geoserver-2.7.0.war',
+    source => 'puppet:///modules/geoserver/geoserver-2.7.0.war',
 	notify => Service['tomcat6'],
   }
