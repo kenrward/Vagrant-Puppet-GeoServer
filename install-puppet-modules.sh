8f4a961bd51ebf9cfbc54f7af72e106087589cee
kenrward <kenrward@gmail.com>
2015-04-15 08:55:15 -0400

    geoserver centos not working

--- install-puppet-modules.sh
+++ install-puppet-modules.sh
@@ -1,5 +1,5 @@
 mkdir -p /etc/puppet/modules;
-yum -y update
+#yum -y update
 if [ ! -d /etc/puppet/modules/java ]; then
 puppet module install puppetlabs-java
 puppet module install puppetlabs-tomcat
